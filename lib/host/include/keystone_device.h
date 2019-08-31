b673c4b54bfc92f6704a2f93936f912149f96777
Alex Thomas <alexthomas@berkeley.edu>
2019-08-30 17:02:06 -0700

    Debug

--- lib/host/include/keystone_device.h
+++ lib/host/include/keystone_device.h
@@ -48,19 +48,19 @@ public:
 
 };
 
-//class MockKeystoneDevice: public KeystoneDeviceInterface{
-//private:
-//    int fd;
-//public:
-//    MockKeystoneDevice() {};
-//    ~MockKeystoneDevice() {};
-//    bool initDevice(Params params);
-//    int ioctl_ioc_create_enclave(struct keystone_ioctl_create_enclave *enclp);
-//    int ioctl_ioc_utm_init(struct keystone_ioctl_create_enclave *enclp);
-//    int ioctl_ioc_finalize_enclave(struct keystone_ioctl_create_enclave *enclp);
-//    int ioctl_destroy_enclave(struct keystone_ioctl_create_enclave *enclp);
-//    int ioctl_run_enclave(struct keystone_ioctl_run_enclave *enclp);
-//    int ioctl_resume_enclave(struct keystone_ioctl_run_enclave *enclp);
-//    int getFD();
-//};
+class MockKeystoneDevice: public KeystoneDeviceInterface{
+private:
+    int fd;
+public:
+    MockKeystoneDevice() {};
+    ~MockKeystoneDevice() {};
+    bool initDevice(Params params);
+    int ioctl_ioc_create_enclave(struct keystone_ioctl_create_enclave *enclp);
+    int ioctl_ioc_utm_init(struct keystone_ioctl_create_enclave *enclp);
+    int ioctl_ioc_finalize_enclave(struct keystone_ioctl_create_enclave *enclp);
+    int ioctl_destroy_enclave(struct keystone_ioctl_create_enclave *enclp);
+    int ioctl_run_enclave(struct keystone_ioctl_run_enclave *enclp);
+    int ioctl_resume_enclave(struct keystone_ioctl_run_enclave *enclp);
+    int getFD();
+};
 
