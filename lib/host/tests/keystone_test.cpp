1d191a77d363e539dd62345a803eb39d1479a0b1
Alex Thomas <alexthomas@berkeley.edu>
2019-08-31 22:07:53 -0700

    Debug

--- lib/host/tests/keystone_test.cpp
+++ lib/host/tests/keystone_test.cpp
@@ -4,14 +4,16 @@
 
 //#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "keystone.h"
 
 //using ::testing::AtLeast;                         // #1
 
-TEST(SquareRootTest, PositiveNos) {
+TEST(KeystoneSDK, PositiveNos) {
 
 }
 
-int main(int argc, char **argv) {
-::testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
