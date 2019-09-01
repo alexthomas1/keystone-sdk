2d30f64a10bce60326e0a138d4d414ec8379cd3d
Alex Thomas <alexthomas@berkeley.edu>
2019-08-31 19:09:41 -0700

    Debug

--- lib/host/tests/keystone_test.cpp
+++ lib/host/tests/keystone_test.cpp
@@ -6,3 +6,10 @@
 #include "gtest/gtest.h"
 
 using ::testing::AtLeast;                         // #1
+
+
+
+int main(int argc, char *argv[]){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
