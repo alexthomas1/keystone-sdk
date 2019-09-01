7de6ff8a6524bc76689fbac06a55885597074ad1
Alex Thomas <alexthomas@berkeley.edu>
2019-09-01 11:21:15 -0700

    Debug

--- tests/keystone_test.cpp
+++ tests/keystone_test.cpp
@@ -15,7 +15,7 @@ size_t untrusted_size = 2*1024*1024;
 size_t freemem_size = 48*1024*1024;
 uintptr_t utm_ptr = (uintptr_t)DEFAULT_UNTRUSTED_PTR;
 
-TEST(Keystone_Init, InvalidELF) {
+TEST(Keystone_Init, ValidInit) {
   Keystone enclave;
   Params params;
 
@@ -23,21 +23,42 @@ TEST(Keystone_Init, InvalidELF) {
   params.setUntrustedMem(utm_ptr, untrusted_size);
   params.setSimulated(true);
 
-//  ASSERT_ANY_THROW(enclave.init("stack.eapp_riscv", 0, params));
-  EXPECT_EQ(enclave.init("test_runner", "test_runner", params), KEYSTONE_ERROR);
+  EXPECT_EQ(enclave.init("test_runner", "test_runner", params), KEYSTONE_SUCCESS);
 }
 
-//TEST(Keystone_Run, RunTest) {
-//  Keystone enclave;
-//  Params params;
-//
-//  params.setFreeMemSize(untrusted_size);
-//  params.setUntrustedMem(utm_ptr, untrusted_size);
-//  params.setSimulated(true);
-//
-//  enclave.init("test_runner", "eyrie-rt", params)
-//  EXPECT_EQ(enclave.run(), KEYSTONE_SUCCESS);
-//}
+TEST(Keystone_Init, NullRT) {
+  Keystone enclave;
+  Params params;
+
+  params.setFreeMemSize(untrusted_size);
+  params.setUntrustedMem(utm_ptr, untrusted_size);
+  params.setSimulated(true);
+
+  ASSERT_ANY_THROW(enclave.init("test_runner", 0, params));
+}
+
+TEST(Keystone_Init, NullEAPP) {
+  Keystone enclave;
+  Params params;
+
+  params.setFreeMemSize(untrusted_size);
+  params.setUntrustedMem(utm_ptr, untrusted_size);
+  params.setSimulated(true);
+
+  ASSERT_ANY_THROW(enclave.init(0, "eyrie-rt", params));
+}
+
+TEST(Keystone_Run, RunTest) {
+  Keystone enclave;
+  Params params;
+
+  params.setFreeMemSize(untrusted_size);
+  params.setUntrustedMem(utm_ptr, untrusted_size);
+  params.setSimulated(true);
+
+  EXPECT_EQ(enclave.init("test_runner", "test_runner", params), KEYSTONE_SUCCESS);
+  EXPECT_EQ(enclave.run(), KEYSTONE_SUCCESS);
+}
 
 
 int main(int argc, char **argv)
