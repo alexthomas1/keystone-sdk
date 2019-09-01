fabad1a96d718dbc48be21919eccf8c24b4c354a
Alex Thomas <alexthomas@berkeley.edu>
2019-09-01 11:08:40 -0700

    Debug

--- tests/keystone_test.cpp
+++ tests/keystone_test.cpp
@@ -15,7 +15,7 @@ size_t untrusted_size = 2*1024*1024;
 size_t freemem_size = 48*1024*1024;
 uintptr_t utm_ptr = (uintptr_t)DEFAULT_UNTRUSTED_PTR;
 
-TEST(KeystoneSDK, InvalidELF) {
+TEST(Keystone_Init, InvalidELF) {
   Keystone enclave;
   Params params;
 
@@ -24,8 +24,21 @@ TEST(KeystoneSDK, InvalidELF) {
   params.setSimulated(true);
 
 //  ASSERT_ANY_THROW(enclave.init("stack.eapp_riscv", 0, params));
-  EXPECT_EQ(enclave.init("test_runner", "eyrie-rt", params), KEYSTONE_ERROR);
+  EXPECT_EQ(enclave.init("test_runner", "test_runner", params), KEYSTONE_ERROR);
 }
+
+TEST(Keystone_Run, RunTest) {
+  Keystone enclave;
+  Params params;
+
+  params.setFreeMemSize(untrusted_size);
+  params.setUntrustedMem(utm_ptr, untrusted_size);
+  params.setSimulated(true);
+
+  enclave.init("test_runner", "eyrie-rt", params)
+  EXPECT_EQ(enclave.run(), KEYSTONE_SUCCESS);
+}
+
 
 int main(int argc, char **argv)
 {
