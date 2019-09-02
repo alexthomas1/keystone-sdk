23fa5f1977723aa54d53bb4c03619ad0d4faf0b8
Alex Thomas <alexthomas@berkeley.edu>
2019-09-02 02:23:44 -0700

    Debug

--- lib/host/include/hash_util.h
+++ lib/host/include/hash_util.h
@@ -115,7 +115,6 @@ int validate_and_hash_epm(hash_ctx_t* hash_ctx, int level,
       /* Do linear mapping validation */
       if(in_runtime){
         if(phys_addr <= *runtime_max_seen){
-          printf("1\n");
           goto fatal_bail;
         }
         else{
@@ -124,7 +123,6 @@ int validate_and_hash_epm(hash_ctx_t* hash_ctx, int level,
       }
       else if(in_user){
         if(phys_addr <= *user_max_seen){
-          printf("2\n");
           goto fatal_bail;
         }
         else{
