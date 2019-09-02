8965d1b46a6ee9362bc8fef3e7bf83f040f32bb2
Alex Thomas <alexthomas@berkeley.edu>
2019-09-02 02:10:25 -0700

    Debug

--- tests/keystone_test.cpp
+++ tests/keystone_test.cpp
@@ -8,12 +8,18 @@
 #include "gtest/gtest.h"
 #include "keystone.h"
 
+#define EYRIE_RT
+#define TEST_EAPP
+
 size_t untrusted_size = 2*1024*1024;
 size_t freemem_size = 48*1024*1024;
 char const *hash = "\xF\x7F\xED\xF5\xEB\x82\x8B\xD2\x11\xFBWv\xBE\xB4m\x80\xD2";
 uintptr_t utm_ptr = (uintptr_t)DEFAULT_UNTRUSTED_PTR;
 
 TEST(Keystone_Init, ValidMeasure) {
+  /* Measures enclave using a simulated enclave
+   *
+   * */
   Keystone enclave;
   Params params;
 
@@ -21,7 +27,7 @@ TEST(Keystone_Init, ValidMeasure) {
   params.setUntrustedMem(utm_ptr, untrusted_size);
   params.setSimulated(true);
 
-  EXPECT_EQ(enclave.init("stack.eapp_riscv", "eyrie-rt", params), KEYSTONE_SUCCESS);
+  EXPECT_EQ(enclave.init(TEST_EAPP, EYRIE_RT, params), KEYSTONE_SUCCESS);
   ASSERT_STREQ(enclave.getHash(), hash);
   EXPECT_EQ(enclave.destroy(), KEYSTONE_SUCCESS);
 }
@@ -34,7 +40,7 @@ TEST(Keystone_Init, NullRT) {
   params.setUntrustedMem(utm_ptr, untrusted_size);
   params.setSimulated(true);
 
-  ASSERT_ANY_THROW(enclave.init("stack.eapp_riscv", 0, params));
+  ASSERT_ANY_THROW(enclave.init(TEST_EAPP, 0, params));
 }
 
 TEST(Keystone_Init, NullEAPP) {
@@ -45,7 +51,7 @@ TEST(Keystone_Init, NullEAPP) {
   params.setUntrustedMem(utm_ptr, untrusted_size);
   params.setSimulated(true);
 
-  ASSERT_ANY_THROW(enclave.init(0, "eyrie-rt", params));
+  ASSERT_ANY_THROW(enclave.init(0, EYRIE_RT, params));
 }
 
 TEST(Keystone_Run, RunTest) {
@@ -56,7 +62,7 @@ TEST(Keystone_Run, RunTest) {
   params.setUntrustedMem(utm_ptr, untrusted_size);
   params.setSimulated(true);
 
-  EXPECT_EQ(enclave.init("stack.eapp_riscv", "eyrie-rt", params), KEYSTONE_SUCCESS);
+  EXPECT_EQ(enclave.init(TEST_EAPP, EYRIE_RT, params), KEYSTONE_SUCCESS);
   EXPECT_EQ(enclave.run(), KEYSTONE_SUCCESS);
 }
 
