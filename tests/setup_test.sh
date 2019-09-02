78dc7436f1436d5498b70ec91417f3fdc6c8e90c
Alex Thomas <alexthomas@berkeley.edu>
2019-09-02 12:22:09 -0700

    Debug

---
+++ tests/setup_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DIR="/usr/src/gtest"
+if [ -d "$DIR" ]; then
+	apt-get install libgtest-dev
+fi
+prevdir=$(pwd)
+GLIB=$DIR/libgtest.so  
+if [ -d "$DIR" ]; then
+	cd $DIR
+	cmake CMakeLists.txt
+	make
+	cp *.a /usr/lib
+	cd $prevdir
+fi
