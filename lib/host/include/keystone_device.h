785a6b5afcaa3bedef6dbaae1fae16558a350d22
Alex Thomas <alexthomas@berkeley.edu>
2019-09-06 21:37:51 -0700

    Seperate mock device from host dir

--- lib/host/include/keystone_device.h
+++ lib/host/include/keystone_device.h
@@ -13,7 +13,6 @@
 #include "params.h"
 
 
-
 class KeystoneDeviceInterface
 {
 private:
@@ -48,19 +47,5 @@ public:
 
 };
 
-class MockKeystoneDevice: public KeystoneDeviceInterface{
-private:
-    int fd;
-public:
-    MockKeystoneDevice() {};
-    ~MockKeystoneDevice() {};
-    bool initDevice(Params params);
-    int ioctl_ioc_create_enclave(struct keystone_ioctl_create_enclave *enclp);
-    int ioctl_ioc_utm_init(struct keystone_ioctl_create_enclave *enclp);
-    int ioctl_ioc_finalize_enclave(struct keystone_ioctl_create_enclave *enclp);
-    int ioctl_destroy_enclave(struct keystone_ioctl_create_enclave *enclp);
-    int ioctl_run_enclave(struct keystone_ioctl_run_enclave *enclp);
-    int ioctl_resume_enclave(struct keystone_ioctl_run_enclave *enclp);
-    int getFD();
-};
+
 
--- lib/host/src/keystone_device.cpp
+++ lib/host/src/keystone_device.cpp
@@ -50,43 +50,5 @@ bool KeystoneDevice::initDevice(Params params)
   return true;
 }
 
-int MockKeystoneDevice::ioctl_ioc_create_enclave(struct keystone_ioctl_create_enclave *enclp)
-{
-  return 0;
-}
 
-int MockKeystoneDevice::ioctl_ioc_utm_init(struct keystone_ioctl_create_enclave *enclp)
-{
-  return 0;
-
-}
-
-int MockKeystoneDevice::ioctl_ioc_finalize_enclave(struct keystone_ioctl_create_enclave *enclp)
-{
-  return 0;
-}
-
-int MockKeystoneDevice::ioctl_destroy_enclave(struct keystone_ioctl_create_enclave *enclp)
-{
-  return 0;
-}
-
-int MockKeystoneDevice::ioctl_run_enclave(struct keystone_ioctl_run_enclave *enclp)
-{
-  return 0;
-}
-
-int MockKeystoneDevice::ioctl_resume_enclave(struct keystone_ioctl_run_enclave *enclp)
-{
-  return 0;
-}
-
-int MockKeystoneDevice::getFD(){
-  return 5;
-}
-
-bool MockKeystoneDevice::initDevice(Params params)
-{
-  return true;
-}
 
